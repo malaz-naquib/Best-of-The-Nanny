aad315c8c4605555428f8e881ae55b737b09824f
Malaz Naquib <malaz.naquib1@gmail.com>
2023-06-28 21:09:27 -0700

    Fix attempt

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,13 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const season = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-app-mn.onrender.com/api/${season}`)
+        const response = await fetch(`https://best-of-the-nanny.onrender.com/api/${season}`)
         const data = await response.json()
 
         console.log(data)
         document.querySelector('h2').innerText = data.season
-        document.querySelector('h2').innerText = data.episode
-        document.querySelector('h2').innerText = data.rating
+        // document.querySelector('h2').innerText = data.episode
+        // document.querySelector('h2').innerText = data.rating
     }catch(error){
         console.log(error)
     }
